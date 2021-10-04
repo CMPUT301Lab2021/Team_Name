b072188d6e11eb5ef7e3d566aff9ef88d3d845fe
Shrey2809 <90151065+Shrey2809@users.noreply.github.com>
2021-10-04 11:07:08 -0600

    Update Circle.java

---
+++ Team_Name/app/src/main/java/com/example/team_name/Circle.java
@@ -0,0 +1,14 @@
+package com.example.team_name;
+
+public class Rectangle extends Shape {
+    Integer x;
+    Integer y;
+    Integer angle;
+    String color;
+    Shape(Integer x, Integer y){
+        this.x = x;
+        this.y = y;
+        angle = 90;
+        color = "blue"
+    }
+}
