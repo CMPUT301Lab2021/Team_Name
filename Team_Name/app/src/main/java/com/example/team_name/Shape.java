32364ff75149e3377afbe500f6fe26b893ce9515
Ivan Seit <seit@ualberta.ca>
2021-10-03 21:42:45 -0600

    msg

--- Team_Name/app/src/main/java/com/example/team_name/Shape.java
+++ Team_Name/app/src/main/java/com/example/team_name/Shape.java
@@ -3,10 +3,13 @@ package com.example.team_name;
 public class Shape  {
     Integer x;
     Integer y;
+    private String Color;
 
-    Shape(Integer x, Integer y){
+    Shape(Integer x, Integer y, String Color){
         this.x = x;
         this.y = y;
+        this.Color=Color;
     }
+    public void setColor(String Color){this.Color="blue";}
 }
 
