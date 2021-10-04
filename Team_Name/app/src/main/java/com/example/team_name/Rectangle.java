6e0617280b1f7a2ec3f83f80f9a976ed356cd668
Shrey Mandol <mandol@ualberta.ca>
2021-10-04 14:42:19 -0600

    Create Rectangle.java

---
+++ Team_Name/app/src/main/java/com/example/team_name/Rectangle.java
@@ -0,0 +1,13 @@
+package com.example.team_name;
+
+public class Rectangle extends Shape {
+    Integer x;
+    Integer y;
+    Integer angle;
+
+    Rectangle(Integer x, Integer y){
+        super(x, y);
+        angle = 90;
+    }
+}
+
