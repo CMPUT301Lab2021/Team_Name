90d0b1bb945f16760e2af895ee4bc4e9e4d319fd
Penelope <penelope0579@gmail.com>
2021-10-03 22:08:00 -0600

    Moon shape commit

---
+++ Team_Name/app/src/main/java/com/example/team_name/Moon.java
@@ -0,0 +1,10 @@
+package com.example.team_name;
+
+public class Moon extends Shape{
+    String color;
+    Moon(String color, Integer x, Integer y) {
+        super(x, y);
+        this.color = color;
+    }
+
+}
