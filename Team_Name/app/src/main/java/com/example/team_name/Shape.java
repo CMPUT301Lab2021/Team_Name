592e9b8d5253c493f36f80f4a5c68ce3fdb51599
pen-ualberta <67674599+pen-ualberta@users.noreply.github.com>
2021-10-04 15:02:21 -0600

    Update Shape.java

--- Team_Name/app/src/main/java/com/example/team_name/Shape.java
+++ Team_Name/app/src/main/java/com/example/team_name/Shape.java
@@ -3,10 +3,13 @@ package com.example.team_name;
 public class Shape {
     Integer x;
     Integer y;
+    String color;
+
 
     Shape(Integer x, Integer y){
         this.x = x;
         this.y = y;
+        this.color = "$PUT_YOUR_CHOICE_OF_COLOR"
     }
 }
 
