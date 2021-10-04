d7342ad60e945385ae89481d499d1dde65d70689
Sagan Girod <lgirod@ualberta.ca>
2021-10-04 13:47:02 -0600

    Create Octagon.java

---
+++ Team_Name/app/src/main/java/com/example/team_name/Octagon.java
@@ -0,0 +1,12 @@
+package com.example.team_name;
+
+public class Octagon extends Shape {
+
+    private int sideLen;
+    final private int sides = 8;
+
+    public Octagon(int sideLen) {
+        this.sideLen = sideLen;
+    }
+    
+}
